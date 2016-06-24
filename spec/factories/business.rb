2c8f1cba8c9670ab450a01a061fd10caf2d316c9
Terry Raimondo <terry.raimondo@gmail.com>
2016-06-24 18:24:38 +0200

    Doc

--- spec/factories/business.rb
+++ spec/factories/business.rb
@@ -1,5 +1,8 @@
 FactoryGirl.define do
   factory :business do
     name { Faker::Company.name }
+    # Uncomment to limit the nb
+    # name { ['Business de poule', 'NASA', 'Dartagnans', 'Maman'].sample }
+    # initialize_with { Group.find_or_create_by_name(name)}
   end
 end
