1707b123d817c4578635b3ca602f873f8b90b7cc
Thibaut Roche <thibaut.roche.perso@gmail.com>
2016-06-27 11:22:35 +0200

    [C]More fields on user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160623133307) do
+ActiveRecord::Schema.define(version: 20160627090612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,13 @@ ActiveRecord::Schema.define(version: 20160623133307) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "address"
+    t.string   "postal_code"
+    t.string   "city"
+    t.string   "phone_number"
+    t.date     "birthdate"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
