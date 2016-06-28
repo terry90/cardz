f56c881ee61d36e59836355d57dfa524149db69d
Thibaut Roche <thibaut.roche.perso@gmail.com>
2016-06-28 13:39:01 +0200

    [C]Front for offers list

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20160627095154) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -84,10 +85,6 @@ ActiveRecord::Schema.define(version: 20160627095154) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
     t.string   "first_name"
     t.string   "last_name"
     t.string   "address"
@@ -95,6 +92,10 @@ ActiveRecord::Schema.define(version: 20160627095154) do
     t.string   "city"
     t.string   "phone_number"
     t.date     "birthdate"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
