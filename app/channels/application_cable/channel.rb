480fe25aa1ad2cbbc379209244b2a047713dcc13
Thibaut Roche <thibaut.roche.perso@gmail.com>
2016-08-10 18:44:26 +0200

    [A]RDoc for channels

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -1,4 +1,6 @@
+# Default module for Action Cable
 module ApplicationCable
+  # Default ActionCable Channel class. All other ActionCable Channel should inherit from it.
   class Channel < ActionCable::Channel::Base
   end
 end
--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,6 @@
+# Default module for Action Cable
 module ApplicationCable
+  # Default ActionCable Connection class. All other ActionCable Connection should inherit from it.
   class Connection < ActionCable::Connection::Base
   end
 end
